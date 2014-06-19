6f0673867a082c8d0128b6881101ee82a0014928
TwisterNo <sorpal12@nith.no>
2014-06-19 19:13:03 +0200

    cart part1 finished

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,9 +26,9 @@ class LineItemsController < ApplicationController
 
   # POST /line_items
   # POST /line_items.json
-  def create product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-    
+  def create product = Product.find(params[:product_id])   
+    session[:counter] = 0 
+    @line_item = @cart.line_items.build(product: product)    
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart,
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  def visit_page_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
+  end
+     
   def index
+    @counter = visit_page_counter
     @products = Product.order(:title)
+    @counterMessage = "The count is:  #{@counter} " if @counter > 5
   end
 end
